10461ca36e9ed24b22356c03f8ce18975112d775
Pessi-L <pessibr@gmail.com>
2021-12-22 19:29:23 +0200

    Set up WhatsApp chatbot

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 from flask import Flask
+from flask import request
+from twilio.rest import Client
+import os
 
 app = Flask(__name__)
 
+ACCOUNT_ID = os.environ.get('TWILIO_ACCOUNT')
+TWILIO_TOKEN = os.environ.get('TWILIO_TOKEN')
+TWILIO_NUMBER = 'whatsapp:+14155238886'
+# initializing a client
+client = Client(ACCOUNT_ID, TWILIO_TOKEN)
 
-@app.route('/')
-def welcome():
-    return {
-        'Result': 'Welcome to WhatsApp tool kit!'
-    }
+
+def process_message(msg):
+    if msg == 'hi':
+        return 'Welcome to WhatsApp tool kit!'
+    else:
+        return 'Please type "hi" to get started'
+
+
+def send_message(msg, recipient):
+    client.messages.create(
+        from_=TWILIO_NUMBER,
+        body=msg,
+        to=recipient
+    )
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    f = request.form
+    msg = f['Body']
+    sender = f['From']
+    response = process_message(msg)
+    send_message(response, sender)
+
+    return 'OK', 200
+
